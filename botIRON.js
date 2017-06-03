67b7ce46d62e752a6a8f628d94ba69395eb5fefd
Texas <fouquett@outlook.com>
2017-06-03 16:54:56 -0300

    Update botIRON.js

--- botIRON.js
+++ botIRON.js
@@ -49,7 +49,7 @@
       botName: "DJ Iron",
       language: "portuguese",
       chatLink: "https://rawgit.com/monteapril/plugHTML/master/langIRON.json",
-      scriptLink: "https://rawgit.com/monteapril/source/master/botIRON.js",
+      scriptLink: "https://rawgit.com/monteapril/plugHTML/master/botIRON.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 200, // 1-200
       startupVolume: 100, // 0-100
@@ -111,6 +111,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/monteapril/source/master/botIRON.js", extend);
+    $.getScript("https://rawgit.com/monteapril/plugHTML/master/botIRON.js", extend);
 
 }).call(this);
